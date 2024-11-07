07035ea256753e10bfef873d7dddc2d8fc1c3ac7
alex64a <aleksamitrovcan@gmail.com>
2024-11-07 10:08:57 +0100

    Initial Commit

---
+++ SwiftUITutorial-SwiftfulThinking/SwiftUITutorial_SwiftfulThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorial_SwiftfulThinkingApp.swift
+//  SwiftUITutorial-SwiftfulThinking
+//
+//  Created by User02 on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorial_SwiftfulThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUITutorial-SwiftfulThinkingTests/SwiftUITutorial_SwiftfulThinkingTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorial_SwiftfulThinkingTests.swift
+//  SwiftUITutorial-SwiftfulThinkingTests
+//
+//  Created by User02 on 11/7/24.
+//
+
+import Testing
+@testable import SwiftUITutorial_SwiftfulThinking
+
+struct SwiftUITutorial_SwiftfulThinkingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
